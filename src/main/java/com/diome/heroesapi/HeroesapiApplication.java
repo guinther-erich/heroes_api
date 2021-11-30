ebfad26d59328c8012a635ec0cc768c4e92f08b6
Guinther Erich <guinther.erich@gmail.com>
2021-11-30 20:30:28 -0300

    Spring Initializr

---
+++ src/main/java/com/diome/heroesapi/HeroesapiApplication.java
@@ -0,0 +1,13 @@
+package com.diome.heroesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diome/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diome.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
