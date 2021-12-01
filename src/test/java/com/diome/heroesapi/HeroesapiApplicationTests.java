0955246a9cd4b4ae5716fbf1043476488baaa454
Guinther Erich <guinther.erich@gmail.com>
2021-11-30 23:50:08 -0300

    Creates test

--- src/test/java/com/diome/heroesapi/HeroesapiApplicationTests.java
+++ src/test/java/com/diome/heroesapi/HeroesapiApplicationTests.java
@@ -1,13 +1,59 @@
 package com.diome.heroesapi;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.Test;
+import com.diome.heroesapi.repository.HeroesRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.diome.heroesapi.constants.HeroesConstant.HEROES_ENDPOINT_LOCAL;
+
+
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
 @SpringBootTest
-class HeroesapiApplicationTests {
+class HeroesApiApplicationTests {
+
+	@Autowired
+	WebTestClient webTestClient;
+
+	@Autowired
+	HeroesRepository heroesRepository;
+
 
 	@Test
-	void contextLoads() {
+	public void getOneHeroesById(){
+
+		webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"10")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody();
+	}
+
+	@Test
+	public void getOneHeroesNotFound(){
+
+		webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"10")
+				.exchange()
+				.expectStatus().isNotFound();
+	}
+
+
+	@Test
+	public void deleteHero(){
+		webTestClient.delete().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"1")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isNotFound()
+				.expectBody(Void.class);
 	}
 
 }
+
+
