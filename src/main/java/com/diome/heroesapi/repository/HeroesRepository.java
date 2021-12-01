a50ba3014b001eecbe336b91e2171a31b1faee8f
Guinther Erich <guinther.erich@gmail.com>
2021-11-30 23:30:59 -0300

    Creates HeroesRepository

---
+++ src/main/java/com/diome/heroesapi/repository/HeroesRepository.java
@@ -0,0 +1,9 @@
+package com.diome.heroesapi.repository;
+
+import com.diome.heroesapi.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+}
